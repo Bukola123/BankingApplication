d1cbb63c2ef21483f5ba361bced440489d694f31
bukola12 <butaiwo@ecobank.com>
2023-01-26 14:40:05 +0100

    Feat: Admin module added

--- src/main/java/com/bankApp/authentication/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bankApp.authentication.repository;
-
-import com.bankApp.authentication.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account,Integer> {
-
-}
